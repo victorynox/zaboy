a476a8b42a37d4acda26c25baba3cbd0b39ed3a0
andrey-makeevka@yandex.ru <A19690108>
2016-09-11 18:17:09 +0300

    just echo

---
+++ www/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// Change to the project root, to simplify resolving paths
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+$container = include 'config/container.php';
+
+echo 'init';
